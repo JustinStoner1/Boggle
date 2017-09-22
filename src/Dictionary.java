86be06035003b6282b3e786b2167f64770587aff
JustinStoner1 <31672859+JustinStoner1@users.noreply.github.com>
2017-09-21 22:03:12 -0600

    Add files via upload

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,15 +1,18 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Dictionary
 {
   String fileName;
+  ArrayList<String> dictionary = new ArrayList<String>();
 
   public Dictionary(String fileName)
   {
     this.fileName = fileName;
 
     // This will reference one line at a time
-    String line = null;
+    String word;
 
     try
     {
@@ -19,23 +22,43 @@ public class Dictionary
       // Always wrap FileReader in BufferedReader.
       BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-      while ((line = bufferedReader.readLine()) != null)
+      while (true)
       {
-        System.out.println(line);
+        word = bufferedReader.readLine();
+        if (word != null)
+        {
+          dictionary.add(word);
+        }
+        else break;
       }
-
       // Always close files.
       bufferedReader.close();
     }
     catch (FileNotFoundException ex)
     {
-      System.out.println("Unable to open file '" + fileName + "'");
+      System.out.println("Dictionary txt file not found");
     }
     catch (IOException ex)
     {
-      System.out.println("Error reading file '" + fileName + "'");
+      System.out.println("An error was encountered while reading in the dictionary");
       // Or we could just do this:
       // ex.printStackTrace();
     }
+  }
+
+  public boolean isInDictionary(String word)
+  {
+    return dictionary.contains(word);
+  }
+
+  public String toString()
+  {
+    String stringRepOfDictionary = "";
+    Iterator<String> dictionaryIterator = dictionary.iterator();
+    while (dictionaryIterator.hasNext())
+    {
+      System.out.println(dictionaryIterator.next());
+    }
+    return stringRepOfDictionary;
   }
 }
--- src/Gui.java
+++ src/Gui.java
@@ -1,18 +1,22 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Gui extends Application
 {
   static String dictionaryFilename = "assets/OpenEnglishWordList.txt";
+  static Dictionary dictionary = new Dictionary(dictionaryFilename);
+
   public static void main(String[] args)
   {
-    Dictionary dictionary = new Dictionary(dictionaryFilename);
-    System.out.println("Da");
+
+    //System.out.println(""+dictionary);
+    //System.out.println(dictionary.isInDictionary("zymometers"));
     launch(args);
   }
 
@@ -20,9 +24,35 @@ public class Gui extends Application
   public void start(Stage primaryStage)
   {
     primaryStage.setTitle("Boggle");
+    StackPane window = new StackPane();
 
-    StackPane root = new StackPane();
-    primaryStage.setScene(new Scene(root, 300, 250));
+    Text messageBox = new Text("Enter a word to see if it is in our dictionary");
+    BorderPane screen = new BorderPane();
+    TextField userInputBar = new TextField();
+    screen.setBottom(userInputBar);
+    screen.setCenter(messageBox);
+    window.getChildren().add(screen);
+
+    userInputBar.setOnKeyPressed(event -> {
+      if(event.getCode() == KeyCode.ENTER)
+      {
+        String userInput = userInputBar.getText();
+        boolean isInDictionary = dictionary.isInDictionary(userInput);
+        if (isInDictionary)
+        {
+          messageBox.setText(userInput + " is in our dictionary");
+        }
+        else
+        {
+          messageBox.setText(userInput + " is not in our dictionary");
+        }
+        //System.out.println("Da");
+        userInputBar.setText("");
+      }
+    });
+
+    primaryStage.setScene(new Scene(window, 300, 100));
     primaryStage.show();
   }
+
 }
